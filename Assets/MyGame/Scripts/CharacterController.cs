4c3e6429be8c93c4e076202714032c25674a8bf3
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2020-09-02 10:17:04 +0200

    Movement 1.1: attach player sprite to mouse cursor position on screen

---
+++ Assets/MyGame/Scripts/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    void Update()
+    {
+        // Die Pixelwerte der Maus werden in die Weltposition umgerechnet
+        Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Wir ziehen aus der 3D Position die für uns relevanten 2D Werte der X und Y Achse
+        Vector2 mousePosWorld2D = new Vector2(mousePosWorld.x, mousePosWorld.y);
+
+        this.gameObject.transform.position = mousePosWorld2D;
+    }
+}
