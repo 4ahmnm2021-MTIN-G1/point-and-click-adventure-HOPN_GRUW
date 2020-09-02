a913b677969af712b00f8c2ba547201d2e667b65
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2020-09-02 18:06:05 +0200

    Interaction 2.2: display commandMenu when clicking on IO and position it at location

--- Assets/MyGame/Scripts/InteractableObject.cs
+++ Assets/MyGame/Scripts/InteractableObject.cs
@@ -20,6 +20,17 @@ public class InteractableObject : MonoBehaviour
 
     void OnMouseDown()
     {
+        ui.commandMenu.SetActive(true);
+        ui.commandMenu.transform.position = GetMousePos();
+    }
 
+    private Vector2 GetMousePos()
+    {
+        // Die Pixelwerte der Maus werden in die Weltposition umgerechnet
+        Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Wir ziehen aus der 3D Position die für uns relevanten 2D Werte der X und Y Achse
+        Vector2 mousePosWorld2D = new Vector2(mousePosWorld.x, mousePosWorld.y);
+
+        return mousePosWorld2D;
     }
 }
--- Assets/MyGame/Scripts/UIManager.cs
+++ Assets/MyGame/Scripts/UIManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public CharacterController character;
     public GameObject commandMenu;
     public Text speakText;
     public Text commandLineText;
