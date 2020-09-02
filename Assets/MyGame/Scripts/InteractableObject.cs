37aba98714170427e16d846c3f09dc73783d9c6a
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2020-09-02 17:32:58 +0200

    Interaction 2.1: display thext while hover over interactable object (IO)

---
+++ Assets/MyGame/Scripts/InteractableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public UIManager ui;
+    public string investigateText;
+
+
+    void OnMouseOver()
+    {
+        ui.commandLineText.text = this.gameObject.name;
+    }
+
+    void OnMouseExit()
+    {
+        ui.commandLineText.text = "-----------------";
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+}
